6ca04da14bd91e444ab9b991152021c93662d62a
jackyoung01 <154318863+jackyoung01@users.noreply.github.com>
2024-10-15 12:59:24 +0800

    Add files via upload

--- ser9257516814.sql
+++ ser9257516814.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： localhost
--- 生成日期： 2024-10-13 20:20:09
+-- 生成日期： 2024-10-15 12:56:23
 -- 服务器版本： 5.7.44-log
 -- PHP 版本： 7.4.33
 
@@ -50,18 +50,25 @@ CREATE TABLE `chat_messages` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `room_id` int(11) NOT NULL,
-  `message` text NOT NULL,
+  `message` text COLLATE utf8mb4_unicode_ci,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- 转存表中的数据 `chat_messages`
 --
 
 INSERT INTO `chat_messages` (`id`, `user_id`, `room_id`, `message`, `created_at`) VALUES
-(29, 1, 3, '123', '2024-10-13 11:54:27'),
-(30, 1, 3, 'nihao', '2024-10-13 12:19:00'),
-(31, 1, 3, '在干嘛', '2024-10-13 12:19:05');
+(51, 1, 7, '😎', '2024-10-14 16:39:37'),
+(52, 1, 7, '6666', '2024-10-14 16:39:40'),
+(53, 8, 7, '😎', '2024-10-14 16:45:48'),
+(54, 8, 7, '6', '2024-10-14 16:45:54'),
+(55, 8, 7, '👍', '2024-10-14 16:45:56'),
+(56, 8, 7, '1', '2024-10-14 17:02:49'),
+(57, 8, 7, '😍', '2024-10-14 17:07:28'),
+(58, 8, 7, '🤓', '2024-10-14 17:08:35'),
+(59, 1, 7, '🤓', '2024-10-14 17:16:35'),
+(60, 1, 7, '🐔', '2024-10-14 17:16:45');
 
 -- --------------------------------------------------------
 
@@ -82,8 +89,9 @@ CREATE TABLE `chat_rooms` (
 --
 
 INSERT INTO `chat_rooms` (`id`, `name`, `created_at`, `created_by`, `password`) VALUES
-(3, 'hello', '2024-10-13 11:15:11', 1, 'e10adc3949ba59abbe56e057f20f883e'),
-(4, 'a', '2024-10-13 11:50:14', 1, 'e10adc3949ba59abbe56e057f20f883e');
+(5, '1', '2024-10-14 05:45:17', 5, 'c4ca4238a0b923820dcc509a6f75849b'),
+(7, 'hello', '2024-10-14 16:34:42', 1, '202cb962ac59075b964b07152d234b70'),
+(8, 'jack', '2024-10-15 02:17:46', 9, '202cb962ac59075b964b07152d234b70');
 
 -- --------------------------------------------------------
 
@@ -106,7 +114,12 @@ INSERT INTO `chat_users` (`id`, `username`, `password`, `created_at`) VALUES
 (1, '123', '$2y$10$8CjyQqsZfa0Q8T6NgmJr3ORTZgnofTlyw5r6j9RITthwf3yKZqpF6', '2024-10-13 05:34:50'),
 (2, '666', '$2y$10$HYdiHb89HdSY7rY49WOtse4I0MB89zWsKdK10D.jnkKRLubEm4qJG', '2024-10-13 05:41:57'),
 (3, '777', '$2y$10$GoyNmCs6hJAc6gcJD3Q0e.Q92oS5HNzlW/3YY0nzuGig0bFfNEF26', '2024-10-13 05:49:15'),
-(4, '6666', '$2y$10$En6chuwssAe9Ur0MsrJkCeALNqdG1L54gV5xFdfC7xkEhOpxyutrW', '2024-10-13 07:41:31');
+(4, '6666', '$2y$10$En6chuwssAe9Ur0MsrJkCeALNqdG1L54gV5xFdfC7xkEhOpxyutrW', '2024-10-13 07:41:31'),
+(5, 'Southerly', '$2y$10$FzR2t5K8MDSjsPi9AELYrekbWnG3hRsxCeI42NfClz7o5mDf.l54O', '2024-10-14 05:44:59'),
+(6, '123456', '$2y$10$odLk4.gaUDgZ4qr4DBMU2..OZ7M5oX9Tnbu9n6LOj72Prr8a4yrIq', '2024-10-14 16:16:38'),
+(7, '111', '$2y$10$uP3JfS7.7VbR6nndAan25.qYkOT5uPbcHHsb5hkAlsndR8wtqNBB2', '2024-10-14 16:39:49'),
+(8, '1111', '$2y$10$PZ99ohPyfiYs9a5RmICbUeMReDL4BZUQrpzAScwBCPNUfXhiSXBQu', '2024-10-14 16:43:51'),
+(9, 'jack', '$2y$10$YiBkeLKdCo7cYZHowC7xR.Y2CjTvmdO/kXa4WV8KrSAcl4d..2MYO', '2024-10-15 02:17:33');
 
 --
 -- 转储表的索引
@@ -154,19 +167,19 @@ ALTER TABLE `admin_users`
 -- 使用表AUTO_INCREMENT `chat_messages`
 --
 ALTER TABLE `chat_messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
 
 --
 -- 使用表AUTO_INCREMENT `chat_rooms`
 --
 ALTER TABLE `chat_rooms`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- 使用表AUTO_INCREMENT `chat_users`
 --
 ALTER TABLE `chat_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- 限制导出的表

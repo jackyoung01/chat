05c72b404620292e8592cd553cb0a0311dc4d68a
jackyoung01 <154318863+jackyoung01@users.noreply.github.com>
2024-10-31 23:30:49 +0800

    Create toggleForm.js

---
+++ src/js/toggleForm.js
@@ -0,0 +1,9 @@
+function showLogin() {
+    document.getElementById('loginForm').classList.remove('hidden');
+    document.getElementById('registerForm').classList.add('hidden');
+}
+
+function showRegister() {
+    document.getElementById('registerForm').classList.remove('hidden');
+    document.getElementById('loginForm').classList.add('hidden');
+}
